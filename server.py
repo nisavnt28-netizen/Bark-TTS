da0e56c0282ae19daa2ff7c576a2e0a418ab8960
nisavnt28-netizen <nisavnt28@gmail.com>
2026-03-10 09:46:31 +0530

    Replace audio generation with TTS model

--- server.py
+++ server.py
@@ -1,23 +1,24 @@
 from flask import Flask, request, send_file
-from bark import generate_audio, preload_models
-from scipy.io.wavfile import write
+from TTS.api import TTS
 import uuid
 
 app = Flask(__name__)
 
-# preload bark model
-preload_models()
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
 
 @app.route("/tts")
-def tts():
+def tts_generate():
     text = request.args.get("text")
 
-    audio = generate_audio(text)
+    file = f"{uuid.uuid4()}.wav"
 
-    filename = f"{uuid.uuid4()}.wav"
+    tts.tts_to_file(
+        text=text,
+        file_path=file,
+        speaker_wav="voice.wav",
+        language="en"
+    )
 
-    write(filename, 24000, audio)
-
-    return send_file(filename, mimetype="audio/wav")
+    return send_file(file)
 
 app.run(host="0.0.0.0", port=10000)
